2ca0e509cc715c1f13be1e95daa86230f2810709
Thomas <dev@thomasbroine.com>
2026-07-16 15:32:06 +0200

    s2-weather-widget-cellule-a-containeriser-base-image-vanilla-user

---
+++ s2-weather-widget-cellule-a-containeriser-base-image-vanilla-user/task.py
@@ -0,0 +1,63 @@
+from ipywidgets.embed import embed_minimal_html
+import os
+import ipywidgets as widgets
+from urllib import request
+import json
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+conf_local_tmp = conf_local_tmp = '/tmp/data'
+conf_cities = conf_cities = {'Amsterdam': (52.37, 4.89), 'Paris': (48.85, 2.35), 'Berlin': (52.52, 13.41), 'Rome': (41.9, 12.5), 'Lisbon': (38.72, -9.14)}
+
+def fetch_json(url):
+    with request.urlopen(url, timeout=15) as resp:
+        return json.loads(resp.read().decode('utf-8'))
+
+def fetch_current(lat, lon):
+    url = (
+        'https://api.open-meteo.com/v1/forecast'
+        f'?latitude={lat}&longitude={lon}'
+        '&current=temperature_2m,relative_humidity_2m,wind_speed_10m'
+        '&timezone=auto'
+        )
+    return fetch_json(url)['current']
+
+os.makedirs(conf_local_tmp, exist_ok=True)
+
+city_names = list(conf_cities.keys())
+tabs_children = []
+for city, (lat, lon) in conf_cities.items():
+    current = fetch_current(lat, lon)
+    info = (
+        f"<b>{current['temperature_2m']} °C</b><br>"
+        f"Humidity: {current['relative_humidity_2m']}%<br>"
+        f"Wind: {current['wind_speed_10m']} km/h"
+        )
+    tabs_children.append(widgets.HTML(value=info))
+
+city_tabs = widgets.Tab(children=tabs_children)
+for i, city in enumerate(city_names):
+    city_tabs.set_title(i, city)
+
+widget_html_path = os.path.join(conf_local_tmp, 'weather_widget.html')
+embed_minimal_html(widget_html_path, views=[city_tabs], title='City weather (ipywidgets)')
+print(f'Wrote {widget_html_path}')
+
+file_widget_html_path = open("/tmp/widget_html_path_" + id + ".json", "w")
+file_widget_html_path.write(json.dumps(widget_html_path))
+file_widget_html_path.close()
